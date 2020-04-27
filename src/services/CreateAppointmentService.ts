61c2a0a4dec3131a5da576a0f999a4bd1d947645
aloisiomartinez <aloisiojacobs36@gmail.com>
2020-04-27 19:26:20 -0300

    First Module

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,4 +1,6 @@
+import { startOfHour } from 'date-fns';
 import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentRepository';
 
 interface Request {
   provider: string;
@@ -6,18 +8,24 @@ interface Request {
 }
 
 class CreateAppointmentService {
-  public execute({ date, provider }: Request): Appointment {
-    const appointmentDate = startOfHour(parsedDate);
+  private appointmentsRepository: AppointmentsRepository;
 
-    const findAppointmentInSameDate = appointmentsRepository.findByDate(
-      parsedDate,
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({ date, provider }: Request): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+      appointmentDate,
     );
 
     if (findAppointmentInSameDate) {
       throw Error('This appointment is already booked');
     }
 
-    const appointment = appointmentsRepository.create({
+    const appointment = this.appointmentsRepository.create({
       provider,
       date: appointmentDate,
     });
