752ff4d7221cf1fc408bb8404183758f30ee49c1
aloisiomartinez <aloisiojacobs36@gmail.com>
2020-04-29 21:01:23 -0300

    New Appointment Model

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,18 +1,15 @@
-import { uuid } from 'uuidv4';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity('appointments')
 class Appointment {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   provider: string;
 
+  @Column('timestamp with time zone')
   date: Date;
-
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    // Omite a variável id com a função Omit, pois a variável id é criada dentro do constructor
-    this.id = uuid();
-    this.provider = provider;
-    this.date = date;
-  }
 }
 
 export default Appointment;
