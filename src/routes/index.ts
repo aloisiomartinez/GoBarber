dd8201d4c885ae579d8cc383d65e69480dc53897
aloisiomartinez <aloisiojacobs36@gmail.com>
2020-05-04 14:42:16 -0300

    JWT

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+
 import appointmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
+import sessionsRouter from './sessions.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
 // Repassa para o appointmentsRouter a rota appointments, desse modo,
 // dentro do arquivo appointments a rota /appointments é padrão
+routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
