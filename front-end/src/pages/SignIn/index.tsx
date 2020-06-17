3a17194d0f3f149e588d034649048448b2f3aabb
aloisiomartinez <aloisiojacobs36@gmail.com>
2020-06-17 15:02:13 -0300

    Hook Toast

--- front-end/src/pages/SignIn/index.tsx
+++ front-end/src/pages/SignIn/index.tsx
@@ -4,7 +4,8 @@ import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
-import { useAuth } from '../../hooks/AuthContext';
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
@@ -23,6 +24,7 @@ const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
   const { signIn } = useAuth();
+  const { addToast } = useToast();
 
   // eslint-disable-next-line @typescript-eslint/ban-types
   const handleSubmit = useCallback(
@@ -51,6 +53,7 @@ const SignIn: React.FC = () => {
           formRef.current?.setErrors(errors);
         }
       }
+      addToast();
     },
     [signIn],
   );
