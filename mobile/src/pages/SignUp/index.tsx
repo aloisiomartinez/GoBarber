9e6f05b1e0836b49bd366bdf4751d13dc2ac65d6
aloisiomartinez <aloisiojacobs36@gmail.com>
2020-07-08 09:45:29 -0300

    Usability on Form

--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -5,6 +5,7 @@ import {
   ScrollView,
   Platform,
   View,
+  TextInput,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
@@ -22,6 +23,10 @@ const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
   const navigation = useNavigation();
+
+  const emailInputRef = useRef<TextInput>(null);
+  const passwordInputRef = useRef<TextInput>(null);
+
   return (
     <>
       <KeyboardAvoidingView
@@ -43,21 +48,33 @@ const SignUp: React.FC = () => {
                 name="name"
                 icon="user"
                 placeholder="Nome"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  emailInputRef.current?.focus();
+                }}
               />
               <Input
+                ref={emailInputRef}
                 keyboardType="email-address"
                 autoCorrect={false}
                 autoCapitalize="none"
                 name="email"
                 icon="mail"
                 placeholder="E-mail"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  passwordInputRef.current?.focus();
+                }}
               />
               <Input
+                ref={passwordInputRef}
                 secureTextEntry
                 name="password"
                 icon="lock"
                 placeholder="Senha"
                 textContentType="newPassword"
+                returnKeyType="send"
+                onSubmitEditing={() => formRef.current?.submitForm()}
               />
 
               <Button
