ecf1d429896d3b6f332d5607c9b2769cddd2d78a
aloisiomartinez <aloisiojacobs36@gmail.com>
2020-09-02 20:52:36 -0300

    Adding Controllers

--- back-end/src/modules/users/infra/http/routes/users.routes.ts
+++ back-end/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,37 +1,24 @@
 import { Router } from 'express';
-import { container } from 'tsyringe';
 
 import multer from 'multer';
 import uploadConfig from '@config/upload';
 
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
-
 import ensureAuthenticate from '../middlewares/ensureAuthenticated';
 
 import UsersController from '../controllers/UsersController';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 const usersController = new UsersController();
-
-const userRouter = Router();
+const userAvatarController = new UserAvatarController();
+const usersRouter = Router();
 const upload = multer(uploadConfig);
 
-userRouter.post('/', usersController.create);
-userRouter.patch(
+usersRouter.post('/', usersController.create);
+usersRouter.patch(
   '/avatar',
   ensureAuthenticate,
   upload.single('avatar'),
-  async (request, response) => {
-    const updateUserAvatar = container.resolve(UpdateUserAvatarService);
-
-    const user = await updateUserAvatar.execute({
-      user_id: request.user.id,
-      avatarFilename: request.file.filename,
-    });
-
-    delete user.password;
-
-    return response.json(user);
-  },
+  userAvatarController.update,
 ); // Atualizar somente 1 informação
 
-export default userRouter;
+export default usersRouter;
