7289ed760181b968e0f0a14b11cda909e08e169e
aloisiomartinez <aloisiojacobs36@gmail.com>
2020-09-07 21:22:05 -0300

    FakeAppointmentRepository

--- back-end/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ back-end/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -8,7 +8,13 @@ import Appointment from '../../infra/typeorm/entities/Appointment';
 class AppointmentsRepository implements IAppointmentsRepository {
   private appointments: Appointment[] = [];
 
-  public async findByDate(date: Date): Promise<Appointment | undefined> {}
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = this.appointments.find(
+      (appointment) => appointment.date === date,
+    );
+
+    return findAppointment;
+  }
 
   public async create({
     provider_id,
--- back-end/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ back-end/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,7 +1,21 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
-  it('shoult be able to create a new appointment', () => {});
+  it('shoult be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '1212121',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('1212121');
+  });
 
   // it('shoult not be able to create two appointments on the same time', () => {});
 });
