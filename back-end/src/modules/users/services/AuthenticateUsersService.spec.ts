c6f480260126a3bd4c81935a8faaea8bf2106c09
aloisiomartinez <aloisiojacobs36@gmail.com>
2020-09-08 22:32:18 -0300

    AuthenticateUser Test

---
+++ back-end/src/modules/users/services/AuthenticateUsersService.spec.ts
@@ -0,0 +1,27 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import AuthenticateUserService from './AuthenticateUserService';
+import CreateUserService from './CreateUserService';
+
+describe('AuthenticateUser', () => {
+  it('shoult be able to authenticate', async () => {
+    const fakeUserRepository = new FakeUsersRepository();
+
+    const createUser = new CreateUserService(fakeUserRepository);
+    const authenticateUser = new AuthenticateUserService(fakeUserRepository);
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    const response = await authenticateUser.execute({
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(response).toHaveProperty('token');
+  });
+});
